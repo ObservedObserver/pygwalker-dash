b08dc2397ea3c3708d53772d35fb5defba7e82da
observedobserver <270001151@qq.com>
2023-10-23 20:19:50 -0700

    feat: support load pre-created visualization

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import dash
-# import dash_core_components as dcc
 import dash_dangerously_set_inner_html
 
 import dash_html_components as html
 import pygwalker as pyg
 
 from datasets import load_dataset
-
 # load dataset
 dataset = load_dataset("gradio/NYC-Airbnb-Open-Data", split="train")
 df = dataset.to_pandas()
 
 app = dash.Dash()
 
-html_code = pyg.walk(df, return_html=True)
+walker = pyg.walk(df, spec="./viz-code.json", debug=False)
+html_code = walker.to_html()
 
 app.layout = html.Div([
     dash_dangerously_set_inner_html.DangerouslySetInnerHTML(html_code),
